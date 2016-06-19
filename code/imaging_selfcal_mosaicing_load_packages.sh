04bf658fdbeee32d90c78cf0c5f6f975b0a66f9b
Anna Ho <annayqho@gmail.com>
2016-06-19 15:01:16 -0600

    initial commit

---
+++ code/imaging_selfcal_mosaicing_load_packages.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/lustre/kmooley/programs/aipslite/parseltongue-2.0-dev/bin/:/lustre/kmooley/programs/aipslite/parseltongue-2.0-dev/bin/:/lustre/kmooley/programs/aipslite/obit-1.1.486-1-64b
+
+export PYTHONPATH=$PYTHONPATH:/lustre/kmooley/programs/aipslite/core:/lustre/kmooley/programs/python/pyfits-3.1/lib/python2.7/site-packages:/lustre/kmooley/programs/python/pywcs-1.11-4.8.2/lib/python2.7/site-packages:/lustre/kmooley/programs/python/pyds9-1.7/installation/lib/python2.6/site-packages:/lustre/kmooley/pipeline/CASA/scripts:/lustre/kmooley/programs/python/pyephem/ephem-3.7.5.3/instl/lib64/python2.6/site-packages
