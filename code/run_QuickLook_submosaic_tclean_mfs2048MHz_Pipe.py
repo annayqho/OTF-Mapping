77c6a5271343ef2976b3d7ab3d692b1141c76d20
Anna Ho <annayqho@gmail.com>
2016-06-20 14:11:56 -0600

    use_script_dir must be set

--- code/run_QuickLook_submosaic_tclean_mfs2048MHz_Pipe.py
+++ code/run_QuickLook_submosaic_tclean_mfs2048MHz_Pipe.py
@@ -84,15 +84,8 @@ tiling='_subtile_%i_%i' % (i_subtile,j_subtile)
 scriptprefix=mydataset+postfix+tiling
 imaging_dir = imaging_dirname+postfix+tiling
 
- 
-if 'use_script_dir' in locals() or 'use_script_dir' in globals():
-    script_dir = use_script_dir
-else:
-    # Current Lustre AOC VLASS smyers area
-    # script_dir = '/lustre/aoc/projects/vlass/smyers/Scripts/'
-    script_dir = 'users/aho/VLA_GW_Followup/Scripts/'
-#script_dir = './' # if you want to use local files
-execfile(script_dir+'getfieldcone.py')
+# use_script_dir is set 
+execfile(use_script_dir+'getfieldcone.py')
 
 # Set up some parameters for processing
 # sdmdir = '/lustre/aoc/projects/vlass/smyers/'
